fabc0f571235e8fb4c1cb720ad352aed27a1925a
Janik Nissen <nissenjanik@gmail.com>
2020-05-14 09:30:46 +0200

    Added Scene: janikTest Added Script: DebugMenu with Restart and Exit functionality Added Script: MouseMovement with 2 MovementModes (Smooth, Direct) Added Prefab: __DEBUG_MANAGER__

---
+++ Breakout of the Pongeon/Assets/MyAssets/Scenes/Debug/DebugMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugMenu : MonoBehaviour
+{
+    //Constss
+
+    //Variables
+    public KeyCode levelRestartKey = KeyCode.R;
+    public KeyCode applicationQuitKey = KeyCode.Escape;
+
+
+    //Methods
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(levelRestartKey)) {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+
+        if (Input.GetKeyDown(applicationQuitKey)) {
+
+            Application.Quit();
+
+        }
+    }
+}
---
+++ Breakout of the Pongeon/Assets/MyAssets/Scenes/Inputs/MouseMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    //Consts
+    public enum MovementMode {SMOOTH, DIRECT}
+
+    //Variables
+    public MovementMode currentMode;
+    public float movementSpeed; //only needed in MovementMode.Smooth
+    private Vector3 newPosition;
+    private Vector3 mousePositionInWorld;
+    private Camera mainCamera;
+    //Methods
+    private void OnEnable() { 
+
+        if (!mainCamera) {
+            mainCamera = Camera.main;
+        }
+    }
+    private void Update() {
+
+        newPosition = transform.position;
+        mousePositionInWorld = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        switch (currentMode) {
+            case MovementMode.DIRECT:
+                newPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+                break;
+            case MovementMode.SMOOTH:
+                if(Mathf.Abs(mousePositionInWorld.x-transform.position.x) <= Time.deltaTime * movementSpeed) {
+
+                    newPosition.x = mousePositionInWorld.x;
+
+                } else {
+                    newPosition.x += 
+                        (mousePositionInWorld.x == transform.position.x? 0f : (mousePositionInWorld.x - transform.position.x) / Mathf.Abs(mousePositionInWorld.x - transform.position.x)) * Time.deltaTime * movementSpeed;
+                }
+                break;
+        }
+
+        transform.position = newPosition;
+    }
+
+
+
+}
