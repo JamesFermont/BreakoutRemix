c354099d009e47cd654a5b326986a189a739d5e4
James <james-edward.ferreira-monteiro@games-academy.de>
2020-05-25 15:45:11 +0200

    Added functionality for block manager, added temp to gitignore
    BlockManager will now handle a blocks attached effects. To add an effect, add the desired effect's script to the block prefab. Supports modes for triggering on hit, on damaged and both.
    
    Also added the Temp/ subfolder to gitignore just in case somebody wants to commit while Unity is still running.

---
+++ Breakout of the Pongeon/Assets/MyAssets/Scripts/Game/Blocks/BlockManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+    public int health = 3;
+
+    public delegate void OnDamaged();
+
+    public event OnDamaged onDamaged;
+
+    public delegate void OnDestroyed();
+
+    public event OnDestroyed onDestroyed;
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        health -= 1;
+
+        if (health <= 0) {
+            onDestroyed?.Invoke();
+            Destroy(this.gameObject);
+        } else {
+            onDamaged?.Invoke();
+        }
+    }
+}
---
+++ Breakout of the Pongeon/Assets/MyAssets/Scripts/Game/Blocks/Effects/TestEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestEffect : MonoBehaviour {
+	public enum EffectType {ON_DAMAGED, ON_DESTROYED, ON_ALL}
+	public EffectType effectType = EffectType.ON_DAMAGED;
+	private void OnEnable() {
+		if (effectType == EffectType.ON_DAMAGED) {
+			//gameObject.GetComponent<BlockManager>().onDamaged -= PerformEffect;
+			gameObject.GetComponent<BlockManager>().onDamaged += PerformEffect;
+		} else if (effectType == EffectType.ON_DESTROYED) {
+			//gameObject.GetComponent<BlockManager>().onDestroyed -= PerformEffect;
+			gameObject.GetComponent<BlockManager>().onDestroyed += PerformEffect;
+		} else {
+			//gameObject.GetComponent<BlockManager>().onDamaged -= PerformEffect;
+			gameObject.GetComponent<BlockManager>().onDamaged += PerformEffect;
+			//gameObject.GetComponent<BlockManager>().onDestroyed -= PerformEffect;
+			gameObject.GetComponent<BlockManager>().onDestroyed += PerformEffect;
+		}
+	}
+
+	public void PerformEffect() {
+		Debug.Log("Noooooooooooooo!");
+	}
+}
