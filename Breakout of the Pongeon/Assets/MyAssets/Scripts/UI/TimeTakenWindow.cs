203fe29f9a192855b90a724e03e3ec2ba95711f3
Janik Nissen <nissenjanik@gmail.com>
2020-07-20 14:26:20 +0200

    TargetTimeUI
    Created a Target Time UI for Lukas to put things into

--- Breakout of the Pongeon/Assets/MyAssets/Scripts/UI/TimeTakenWindow.cs
+++ Breakout of the Pongeon/Assets/MyAssets/Scripts/UI/TimeTakenWindow.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimeTakenWindow : MonoBehaviour
-{
+public class TimeTakenWindow : MonoBehaviour {
     public Transform levelList;
     public Object levelTimePrefab;
 
     string[] levels;
     List<GameObject> entries;
 
-    
-    void OnEnable() {
-        //
-        GameObject currentLevelSign;
-    }
 
-    
+    void OnEnable() {
+        if (levels == null)
+            levels = LevelIO.getLevelsInDirectory();
+
+        levelList.GetComponent<RectTransform>().sizeDelta = new Vector2(960, levels.Length * 72);
+        GameObject currentLevelSign;
+
+        for (int i = 0; i < levels.Length; i++) {
+            currentLevelSign = (GameObject)Instantiate(levelTimePrefab, levelList);
+            currentLevelSign.transform.localPosition = Vector3.down * (i + 0.5f) * 72;
+            currentLevelSign.transform.GetChild(0).GetComponent<TMPro.TMP_Text>().text = levels[i];
+            currentLevelSign.transform.GetChild(1).GetComponent<TMPro.TMP_InputField>().text = LevelTimeTargets.getTarget(levels[i]).ToString();
+            string name = levels[i];
+            currentLevelSign.transform.GetChild(1).GetComponent<TMPro.TMP_InputField>().onSubmit.AddListener();
+        }
+    }
 
 
     private void updateLists () {
