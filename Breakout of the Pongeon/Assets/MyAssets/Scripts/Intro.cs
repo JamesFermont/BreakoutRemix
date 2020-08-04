1c17f538043598e8388a37b58c0c1b7f10bd263c
James <contact@jamesfermont.com>
2020-08-04 09:36:38 +0200

    Implemented Intro

---
+++ Breakout of the Pongeon/Assets/MyAssets/Scripts/Intro.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Intro : MonoBehaviour {
+    [SerializeField] private VideoPlayer vp;
+    
+    private void Start() {
+        vp.loopPointReached += LoadMainMenu;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    private void LoadMainMenu(VideoPlayer vp) {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
